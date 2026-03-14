30255214cf7f3d4ec970e916dad5da144bdac091
Petra Kirschova <kirschovapetra@gmail.com>
2026-03-14 18:58:19 +0100

    react + next.js + eslint + prettier init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,35 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { FlatCompat } from '@eslint/eslintrc';
+import recommendedConfig from 'eslint-plugin-prettier/recommended';
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  ...recommendedConfig,
+  ...compat.config({
+    extends: ['next', 'prettier'],
+    rules: {
+      'no-console': 'warn', // Warning for console.log
+      quotes: ['error', 'double'], // Enforce double quotes
+      'prettier/prettier': 'error', // Prettier errors show up in ESLint output
+    },
+  }),
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'node_modules/**',
   ]),
 ]);
 
